a0275d42ed4fa5216b62ee9ed2df2f1dcc714d69
Bahram1367 <s.bahram67@gmail.com>
2026-04-23 23:16:31 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+python
+# تنظیمات اختصاصی سجاد
+TOKEN = "توکن_ربات_بله_شما"
+ADMIN_ID = "آیدی_عددی_شما" # برای ارسال فاکتورها به شما
+‌
+# لیست برندهای نقدی
+CASH_BRANDS = ["Hafner", "Optibelt", "Visiun", "Mashita", "Click"]
